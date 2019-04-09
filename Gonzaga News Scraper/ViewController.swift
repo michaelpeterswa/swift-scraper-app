2634cdc37120f946d52011f428f7cc702698e08e
Peters, Michael E <mpeters4@cjca-21757l.gonzaga.edu>
2019-04-09 00:38:28 -0700

    Initial Commit

---
+++ Gonzaga News Scraper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gonzaga News Scraper
+//
+//  Created by Peters, Michael E. on 4/9/19.
+//  Copyright © 2019 northw.st. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Gonzaga News ScraperTests/Gonzaga_News_ScraperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gonzaga_News_ScraperTests.swift
+//  Gonzaga News ScraperTests
+//
+//  Created by Peters, Michael E. on 4/9/19.
+//  Copyright © 2019 northw.st. All rights reserved.
+//
+
+import XCTest
+@testable import Gonzaga_News_Scraper
+
+class Gonzaga_News_ScraperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
