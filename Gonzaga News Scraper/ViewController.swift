282cf8b6e2e5e15b9aef26c19215aa2086515f5b
Peters, Michael E <mpeters4@cjca-21757l.gonzaga.edu>
2019-04-09 02:18:24 -0700

    added WebKit and WKWebView and created application that reaches https://www.gonzaga.edu on opening

--- Gonzaga News Scraper/ViewController.swift
+++ Gonzaga News Scraper/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        let url = URL(string: "https://www.gonzaga.edu")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
 
 }
 
